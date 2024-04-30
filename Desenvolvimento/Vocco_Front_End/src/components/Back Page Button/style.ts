4b05dd13c6979dec2e62d8d010eeb71f94f55308
Isadora Vieira <isavieiracamara@gmail.com>
2024-04-29 21:05:52 -0300

    feat(frontend): create initial project struct

---
+++ Desenvolvimento/Vocco_Front_End/src/components/Back Page Button/style.ts
@@ -0,0 +1,11 @@
+// estilização do componente botão de voltar para a página anterior
+import styled from "styled-components";
+
+export const BackPageButton = styled.button `
+padding: 10px 20px;
+border: none;
+border-radius: 4px;
+cursor: pointer;
+background-color: grey;
+color: white;
+`
---
+++ Desenvolvimento/Vocco_Front_End/src/containers/Institution Update/index.tsx
@@ -0,0 +1,14 @@
+import { Title } from "./styles" // importando o titulo estilizado para o componente da página
+import { BackPageButton } from "../../components/Back Page Button/style" // importando componente de botão reutilizável
+
+// componente base dessa página
+function InstitutionUpdate() {
+    return (
+        <div>
+            <Title>Atualização de Instituição</Title>
+            <BackPageButton>Voltar</BackPageButton>
+        </div>
+    )
+}
+
+export default InstitutionUpdate
