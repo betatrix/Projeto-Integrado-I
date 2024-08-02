81572620ca97a54f04119041b93fb96cd01f74c6
Letícia Baião <102307053+LeBaiao@users.noreply.github.com>
2024-08-02 15:39:03 -0300

    comandos dp git bash usado nos vídeos do gource

---
+++ Gource/comandos-gource.sh
@@ -0,0 +1,4 @@
+$ cd projeto-integrado-back
+$ gource log-do-gource --key --user-image-dir "C:\Users\letic\Projeto-Integrado-Back\imagens-usuarios" --caption-file "C:\Users\letic\Projeto-Integrado-Back\caption.txt" --caption-duration 5
+$cd projeto-integrado-front
+$ gource log-do-gource-front --key --user-image-dir "C:\Users\letic\Projeto-Integrado-Front\imagens-usuarios" --caption-file "C:\Users\letic\Projeto-Integrado-Front\caption.txt" --caption-duration 5
